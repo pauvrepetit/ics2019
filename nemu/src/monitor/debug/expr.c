20e6c6d02749a58f0bd5440003b900e6c51b6eb4
tracer-ics2019 <tracer@njuics.org>
2020-10-03 09:49:53 +0800

    >  compile U201714761 root Linux archlinux 5.8.12-arch1-1 #1 SMP PREEMPT Sat, 26 Sep 2020 21:42:58 +0000 x86_64 GNU/Linux  09:49:52 up 1 min,  1 user,  load average: 2.55, 0.87, 0.32 f8332c796dc33081fe9a28f1e9e5b81832a3a4f4

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -9,7 +9,7 @@
 #include <stdlib.h>
 
 enum {
-  TK_NOTYPE = 256, TK_EQ, TK_NUM
+  TK_NOTYPE = 256, TK_EQ, TK_NUM, TK_NEQ, TK_AND, TK_REG
 
   /* TODO: Add more token types */
 
@@ -32,7 +32,10 @@ static struct rule {
   {"/", '/'},
   {"\\(", '('},
   {"\\)", ')'},
-  {"[0-9]+", TK_NUM}
+  {"[0-9]+", TK_NUM},
+  {"!=", TK_NEQ},
+  {"&&", TK_AND},
+  {"$...", TK_REG}
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
@@ -113,13 +116,26 @@ static bool make_token(char *e) {
               return false;
             }
             tokens[nr_token].type = TK_NUM;
+            memset(tokens[nr_token].str, 0, sizeof(char) * 32);
             memcpy(tokens[nr_token].str, substr_start, substr_len);
             tokens[nr_token].loc = position - substr_len;
             nr_token++;
             break;
           case TK_NOTYPE:
             break;
-          // default: TODO();
+          case TK_REG:
+            if (substr_len != 4) {
+              position -= substr_len;
+              printf("no such register at position %d\n%s\n%*.s^\n", position, e, position, "");
+              return false;
+            } else {
+              tokens[nr_token].type = TK_REG;
+              memset(tokens[nr_token].str, 0, sizeof(char) * 32);
+              memcpy(tokens[nr_token].str, substr_start, substr_len);
+              tokens[nr_token].loc = position - substr_len;
+              nr_token++;
+            }
+            break;
           default:
             tokens[nr_token].type = rules[i].token_type;
             memset(tokens[nr_token].str, 0, sizeof(char) * 32);
@@ -147,35 +163,60 @@ typedef struct numToken {
   int loc;
 } numToken;
 
+uint32_t getNum(numToken *e, int len, bool *success, int *offset, char *e_str) {
+  if (len == 0) {
+    *success = false;
+    printf("need a number at position %d\n%s\n%*.s^\n", (e-1)->loc, e_str, (e-1)->loc, "");
+    return 0;
+  }
+  (*offset)++;
+  if (e->type == TK_NUM) {
+    return e->num;
+  } else if (e->type == '+') {
+    return getNum(e+1, len-1, success, offset, e_str);
+  } else if (e->type == '-') {
+    return -getNum(e+1, len-1, success, offset, e_str);
+  } else if (e->type == '*') {
+    uint32_t addr = getNum(e+1, len-1, success, offset, e_str);
+    return *(uint32_t*)(pmem+addr);
+  } else {
+    *success = false;
+    printf("unexcepted symbol at position %d\n%s\n%*.s^\n", e->loc, e_str, e->loc, "");
+    return 0;
+  }
+}
+
+uint32_t getOp(numToken *e, int len, bool *success, int *offset, char *e_str) {
+  if (e->type == TK_NUM) {
+    *success = false;
+    printf("unexcepted number at position %d\n%s\n%*.s^\n", e->loc, e_str, e->loc, "");
+    return 0;
+  } else {
+    *offset = 1;
+    return e->type;
+  }
+}
+
 uint32_t basic_cal_expr(numToken *e, int len, bool *success, char *e_str) {
   numToken *tokenStack = (numToken *)malloc(sizeof(numToken) * len);
   int top = 0;
 
-  int negFlag = 1;
-  bool calFlag = true;
+  // int negFlag = 1;
+  // int refCount = 0;
+  // bool calFlag = true;
 
   *success = true;
 
-  for(int i = 0; i < len; i++) {
-    numToken t = e[i];
-    if (t.type == TK_NUM) {
-      if (calFlag == false) {
-        *success = false;
-        printf("need a math operator at position %d\n%s\n%*.s^\n", t.loc, e_str, t.loc, "");
-        break;
-      }
+  bool odd = true;
+  while(len != 0) {
+    int offset = 0;
+    if (odd) {
+      tokenStack[top].num = getNum(e, len, success, &offset, e_str);
       tokenStack[top].type = TK_NUM;
-      tokenStack[top].num = negFlag * t.num;
-      tokenStack[top].loc = t.loc;
+      tokenStack[top].loc = e->loc;
       top++;
-      calFlag = false;
-      negFlag = 1;
-      // 这里加入了一个数到栈中，我们考虑进行一些操作，如果说这个数的前一个位置是一个乘号/除号，那么就在这进行一次计算，加减的话就不管
-      // 最终处理完成以后，栈中就全部是加减法了
-      if (top == 1) {
-        // 该数是栈中的第一个元素，不处理
-        continue;
-      } else {
+      // 当进来一个数字的时候，我们将乘除法处理掉
+      if (top > 1) {
         if (tokenStack[top-2].type == '*') {
           tokenStack[top-3].num = tokenStack[top-3].num * tokenStack[top-1].num;
           top -= 2;
@@ -186,48 +227,28 @@ uint32_t basic_cal_expr(numToken *e, int len, bool *success, char *e_str) {
           continue;
         }
       }
-    } else if (t.type == '+') {
-      if (calFlag) {
-        continue;
-      } else {
-        tokenStack[top].type = '+';
-        tokenStack[top++].loc = t.loc;
-        calFlag = true;
-      }
-    } else if (t.type == '-') {
-      if (calFlag) {
-        negFlag = -negFlag;
-        continue;
-      } else {
-        tokenStack[top].type = '-';
-        tokenStack[top++].loc = t.loc;
-        calFlag = true;
-      }
-    } else if (t.type == '*' || t.type == '/') {
-      if (calFlag) {
-        *success = false;
-        printf("unexpected %c at position %d\n%s\n%*.s^\n", t.type, t.loc, e_str, t.loc, "");
-        break;
-      } else {
-        tokenStack[top].type = t.type;
-        tokenStack[top++].loc = t.loc;
-        calFlag = true;
-      }
     } else {
-      // todo: 此处后续可以增加更多的对type的处理
+      tokenStack[top].type = getOp(e, len, success, &offset, e_str);
+      tokenStack[top].loc = e->loc;
     }
+    if (!*success) {
+      free(tokenStack);
+      return 0;
+    }
+    e += offset;
+    len -= offset;
+    odd = !odd;
   }
 
-  if (top == 0) {
+  if (odd) {
     *success = false;
-    printf("unexpected %c at position %d\n%s\n%*.s^\n", e[0].type, e[0].loc, e_str, e[0].loc, "");
-  }
-
-  if (!*success) {
+    printf("unexcepted operator at position %d\n%s\n%*.s^\n", (e-1)->loc, e_str, (e-1)->loc, "");
     free(tokenStack);
     return 0;
   }
 
+  // 至此，乘除法计算完成，下面计算加减法
+  int newTop = 0;
   int result = tokenStack[0].num;
   for(int i = 1; i < top; i += 2) {
     numToken t = tokenStack[i];
@@ -235,10 +256,146 @@ uint32_t basic_cal_expr(numToken *e, int len, bool *success, char *e_str) {
       result += tokenStack[i+1].num;
     } else if (t.type == '-') {
       result -= tokenStack[i+1].num;
+    } else {
+      // 不是加减法，==/!=/&&
+      tokenStack[newTop].num = result;
+      tokenStack[newTop].type = TK_NUM;
+      tokenStack[newTop].loc = t.loc-1;
+      newTop++;
+      tokenStack[newTop].type = t.type;
+      tokenStack[newTop].loc = t.loc;
+      newTop++;
+      result = tokenStack[i+1].num;
     }
   }
+  tokenStack[newTop].num = result;
+  tokenStack[newTop].type = TK_NUM;
+  tokenStack[newTop].loc = tokenStack[top-1].loc;
+  newTop++;
+  // 至此，加减法计算完成，余下部分都是逻辑运算 或者 就是计算结果
+  result = tokenStack[0].num;
+  for(int i = 1; i < newTop; i += 2) {
+    numToken t = tokenStack[i];
+    switch (t.type) {
+    case TK_EQ:
+      result = result == tokenStack[i+1].num;
+      break;
+    case TK_NEQ:
+      result = result != tokenStack[i+1].num;
+      break;
+    case TK_AND:
+      result = result && tokenStack[i+1].num;
+      break;
+    default:
+      printf("unexcepted num at position %d\n%s\n%*.s^\n", t.loc, e_str, t.loc, "");
+      break;
+    }
+  }
+  
   free(tokenStack);
   return result;
+
+
+
+
+
+  // for(int i = 0; i < len; i++) {
+  //   numToken t = e[i];
+  //   if (t.type == TK_NUM) {
+  //     if (calFlag == false) {
+  //       *success = false;
+  //       printf("need a math operator at position %d\n%s\n%*.s^\n", t.loc, e_str, t.loc, "");
+  //       break;
+  //     } else if (refCount != 0) {
+  //       // 对这个数字，其前面存在refCount次解引用操作，我们执行这些操作得到最终的操作数
+
+  //     }
+  //     tokenStack[top].type = TK_NUM;
+  //     tokenStack[top].num = negFlag * t.num;
+  //     tokenStack[top].loc = t.loc;
+  //     top++;
+  //     calFlag = false;
+  //     negFlag = 1;
+  //     // 这里加入了一个数到栈中，我们考虑进行一些操作，如果说这个数的前一个位置是一个乘号/除号，那么就在这进行一次计算，其他的运算的话就不管
+  //     // 最终处理完成以后，栈中就是加减法/==/!=/&&
+  //     if (top == 1) {
+  //       // 该数是栈中的第一个元素，不处理
+  //       continue;
+  //     } else {
+  //       if (tokenStack[top-2].type == '*') {
+  //         tokenStack[top-3].num = tokenStack[top-3].num * tokenStack[top-1].num;
+  //         top -= 2;
+  //       } else if (tokenStack[top-2].type == '/') {
+  //         tokenStack[top-3].num = tokenStack[top-3].num / tokenStack[top-1].num;
+  //         top -= 2;
+  //       } else {
+  //         continue;
+  //       }
+  //     }
+  //   } else if (t.type == '+') {
+  //     if (calFlag) {
+  //       continue;
+  //     } else {
+  //       tokenStack[top].type = '+';
+  //       tokenStack[top++].loc = t.loc;
+  //       calFlag = true;
+  //     }
+  //   } else if (t.type == '-') {
+  //     if (calFlag) {
+  //       negFlag = -negFlag;
+  //       continue;
+  //     } else {
+  //       tokenStack[top].type = '-';
+  //       tokenStack[top++].loc = t.loc;
+  //       calFlag = true;
+  //     }
+  //   } else if (t.type == '*') {
+  //     if (calFlag) {
+  //       // * 作为解引用运算符
+  //       refCount++;
+  //       continue;
+  //     } else {
+  //       // * 作为乘法运算符
+  //       tokenStack[top].type = t.type;
+  //       tokenStack[top++].loc = t.loc;
+  //       calFlag = true;
+  //     }
+  //   } else if (t.type == '/') {
+  //     if (calFlag) {
+  //       *success = false;
+  //       printf("unexpected %c at position %d\n%s\n%*.s^\n", t.type, t.loc, e_str, t.loc, "");
+  //       break;
+  //     } else {
+  //       tokenStack[top].type = t.type;
+  //       tokenStack[top++].loc = t.loc;
+  //       calFlag = true;
+  //     }
+  //   } else {
+  //     // todo: 此处后续可以增加更多的对type的处理
+  //   }
+  // }
+
+  // if (top == 0) {
+  //   *success = false;
+  //   printf("unexpected %c at position %d\n%s\n%*.s^\n", e[0].type, e[0].loc, e_str, e[0].loc, "");
+  // }
+
+  // if (!*success) {
+  //   free(tokenStack);
+  //   return 0;
+  // }
+
+  // int result = tokenStack[0].num;
+  // for(int i = 1; i < top; i += 2) {
+  //   numToken t = tokenStack[i];
+  //   if (t.type == '+') {
+  //     result += tokenStack[i+1].num;
+  //   } else if (t.type == '-') {
+  //     result -= tokenStack[i+1].num;
+  //   }
+  // }
+  // free(tokenStack);
+  // return result;
 }
 
 
@@ -299,12 +456,36 @@ uint32_t cal_expr(int l, int r, bool *success, char *e) {
       tokenStack[top].loc = newToken.loc;
       if (newToken.type == TK_NUM) {
         tokenStack[top].num = atoi(newToken.str);
+      } else if (newToken.type == TK_REG) {
+        // 在这里读出寄存器的内容，将TK_REG直接转换成TK_NUM
+        tokenStack[top].type = TK_NUM;
+        if (strcmp(newToken.str, "$EAX") == 0) {
+          tokenStack[top].num = cpu.eax;
+        } else if (strcmp(newToken.str, "$EBX") == 0) {
+          tokenStack[top].num = cpu.ebx;
+        } else if (strcmp(newToken.str, "$ECX") == 0) {
+          tokenStack[top].num = cpu.ecx;
+        } else if (strcmp(newToken.str, "$EDX") == 0) {
+          tokenStack[top].num = cpu.edx;
+        } else if (strcmp(newToken.str, "$ESI") == 0) {
+          tokenStack[top].num = cpu.esi;
+        } else if (strcmp(newToken.str, "$EDI") == 0) {
+          tokenStack[top].num = cpu.edi;
+        } else if (strcmp(newToken.str, "$ESP") == 0) {
+          tokenStack[top].num = cpu.esp;
+        } else if (strcmp(newToken.str, "$EBP") == 0) {
+          tokenStack[top].num = cpu.ebp;
+        } else if (strcmp(newToken.str, "$EPC") == 0) {
+          tokenStack[top].num = cpu.pc;
+        } else {
+          printf("unknown register at position %d\n%s\n%*.s^\n", newToken.loc, e, newToken.loc, "");
+        }
       }
       top++;
     }
   }
 
-  // 至此，tokenStack中仅包含加减乘除和数字 去除了所有的括号
+  // 至此，tokenStack中仅包含加减乘除和数字，还有==/!=/&&，去除了所有的括号
   return basic_cal_expr(tokenStack, top, success, e);
 }
 
