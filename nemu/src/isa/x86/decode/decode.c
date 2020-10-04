70f233293d578bb600aaac04c3309ccd3f92cecb
tracer-ics2019 <tracer@njuics.org>
2020-10-04 22:03:46 +0800

    >  compile U201714761 root Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  22:03:44 up  7:13,  1 user,  load average: 0.25, 0.17, 0.12 9cc9a3c1db0505c92dcbed5e31882bc29c23aeb2

--- nemu/src/isa/x86/decode/decode.c
+++ nemu/src/isa/x86/decode/decode.c
@@ -338,8 +338,9 @@ make_DHelper(sub) {
 make_DHelper(xor) {
   // printf("hit here xor\n");
   decode_op_rm(pc, id_dest, true, NULL, false);
-  decode_op_r(pc, id_dest, true);
-  decode_op_r(pc, id_src, true);
+  decode_op_rm(pc, id_src, true, NULL, false);
+  // decode_op_r(pc, id_dest, true);
+  // decode_op_r(pc, id_src, true);
 }
 
 make_DHelper(ret) {
