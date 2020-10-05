a20c302a014d3eb9067e993a42bdcfd1ecc33a12
tracer-ics2019 <tracer@njuics.org>
2020-10-05 20:33:19 +0800

    >  compile U201714761 root Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  20:33:18 up 48 min,  1 user,  load average: 1.06, 0.39, 0.24 f6f33659b2b60805bd744ebfc24f698c1d32322

--- nemu/src/isa/x86/exec/logic.c
+++ nemu/src/isa/x86/exec/logic.c
@@ -5,6 +5,7 @@ make_EHelper(test) {
   // TODO();
   rtl_and(&t0, &id_dest->val, &id_src->val);
   operand_write(id_dest, &t0);
+  rtl_update_ZFSF(&t0, id_dest->width);
   t0 = 0;
   rtl_set_CF(&t0);
   rtl_set_OF(&t0);
