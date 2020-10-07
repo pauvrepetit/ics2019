2ea7c624983da8e933edf4f5d9294659712386ae
tracer-ics2019 <tracer@njuics.org>
2020-10-07 14:06:01 +0800

    >  compile U201714761 root Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  14:06:00 up 36 min,  1 user,  load average: 0.47, 0.35, 0.28 7936044accd62e1ec958a740b72488c73ca4d1bc

--- nemu/src/isa/x86/decode/decode.c
+++ nemu/src/isa/x86/decode/decode.c
@@ -377,7 +377,7 @@ make_DHelper(movzb) {
 
 make_DHelper(jcc) {
   id_dest->width = 1;
-  decode_op_I(pc, id_dest, true);
+  decode_op_SI(pc, id_dest, true);
   // printf("jcc id_dest.val == %d, pc == %d\n", id_dest->val, *pc);
   id_dest->val += *pc;
   decinfo.jmp_pc = id_dest->val;
