8f39381572071b55fce80a0c375971b979157575
tracer-ics2019 <tracer@njuics.org>
2020-10-07 20:06:39 +0800

    >  compile U201714761 root Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  20:06:38 up  6:03,  1 user,  load average: 0.46, 0.74, 0.69 f5a335f3c55345226988de34b5d94064812560d9

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -154,7 +154,7 @@ static OpcodeEntry opcode_table [512] = {
   /* 0x80 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x84 */	EMPTY, IDEX(jcc, jcc), EMPTY, EMPTY,
   /* 0x88 */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0x8c */	EMPTY, EMPTY, EMPTY, EMPTY,
+  /* 0x8c */	EMPTY, EMPTY, IDEX(jcc, jcc), EMPTY,
   /* 0x90 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x94 */	IDEXW(setcc, setcc, 1), IDEXW(setcc, setcc, 1), EMPTY, EMPTY,
   /* 0x98 */	EMPTY, EMPTY, EMPTY, EMPTY,
