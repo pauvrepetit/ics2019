7414c10fdb3866bf8e1eac71c5b3470333321e36
tracer-ics2019 <tracer@njuics.org>
2020-10-07 20:41:15 +0800

    >  compile U201714761 root Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  20:41:14 up  6:38,  1 user,  load average: 0.60, 0.57, 0.46 947deb6e381a9c64cfe4ef208171e27ef2bef231

--- nemu/src/isa/x86/exec/control.c
+++ nemu/src/isa/x86/exec/control.c
@@ -51,12 +51,8 @@ make_EHelper(ret_imm) {
 
 make_EHelper(call_rm) {
   // TODO();
-  printf("call_rm, now pc is %08x\n", *pc);
-  printf("call_rm, id_dest is %08x, %s\n", id_dest->val, id_dest->str);
-  rtl_lm(&t0, &id_dest->val, id_dest->width);
-  printf("* is %08x\n", t0);
   decinfo_set_jmp(true);
-  decinfo.jmp_pc = t0;
+  decinfo.jmp_pc = id_dest->val;
   rtl_j(decinfo.jmp_pc);
 
   print_asm("call *%s", id_dest->str);
