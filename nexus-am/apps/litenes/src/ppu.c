949ba7fcbea9c4e9f336fb2f8fcaf674d434963e
tracer-ics2019 <tracer@njuics.org>
2020-10-08 23:11:37 +0800

    >  compile U201714761 huao Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  23:11:37 up  6:38,  2 users,  load average: 1.69, 1.96, 2.37 197e14ed87c340a9f777a668d85d18ee1b25cab

--- nexus-am/apps/litenes/src/ppu.c
+++ nexus-am/apps/litenes/src/ppu.c
@@ -254,24 +254,17 @@ void ppu_cycle() {
   TIME_TYPE t0, t1, t2, t3, t4, t5;
 #endif
 
-  // if (ppu.ready) {
-  //   printf("ppu.ready\n");
-  // } else {
-  //   printf("!ppu.ready\n");
-  // }
-  // printf("%d\n", cpu_clock());
   unsigned long long ccl = cpu_clock();
-  printf("%d\n", ccl);
-  if (ccl > 30000) {
-    printf("ccl\n");
+  if (!ppu.ready) {
+    if (ccl > 29658) {
+      printf("???\n");
+      ppu.ready = true;
+    }
   }
-  // if (!ppu.ready) {
-  //   printf("!ppu.ready\n");
+  // if ((!ppu.ready) && (cpu_clock() > 29658)) {
+  //   printf("???\n");
+  //   ppu.ready = true;
   // }
-  if ((!ppu.ready) && (cpu_clock() > 29658)) {
-    printf("???\n");
-    ppu.ready = true;
-  }
   
   // printf("cpu_clock is %d\n", cpu_clock());
   // if (ppu.ready) {
