696ad4ed924106891229f8ea979b2869493ddca4
tracer-ics2019 <tracer@njuics.org>
2020-10-08 19:10:00 +0800

    >  run U201714761 huao Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  19:10:00 up  2:37,  2 users,  load average: 0.91, 0.90, 0.83 92e146383c1f37c9ea62a3a9c3759aa15467d66b

--- nexus-am/apps/microbench/src/bench.c
+++ nexus-am/apps/microbench/src/bench.c
@@ -61,6 +61,7 @@ static unsigned long score(Benchmark *b, unsigned long tsc, unsigned long msec) 
 
 int main(const char *args) {
   const char *setting_name = args;
+  printf("start: args == '%s'\n", args);
   if (args == NULL || strcmp(args, "") == 0) {
     printf("Empty mainargs. Use \"ref\" by default\n");
     setting_name = "ref";
