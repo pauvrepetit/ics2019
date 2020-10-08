82e9950015613a9da14985f0e302a262b472ed80
tracer-ics2019 <tracer@njuics.org>
2020-10-08 19:31:06 +0800

    >  run U201714761 huao Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  19:31:06 up  2:58,  2 users,  load average: 0.57, 0.75, 0.74 880c7abab9f2215dbef280c15c320ef87855b3

--- nexus-am/am/src/nemu-common/nemu-input.c
+++ nexus-am/am/src/nemu-common/nemu-input.c
@@ -3,13 +3,15 @@
 #include <nemu.h>
 
 #define KEYDOWN_MASK 0x8000
+#define I8042_DATA_PORT 0x60
 
 size_t __am_input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       _DEV_INPUT_KBD_t *kbd = (_DEV_INPUT_KBD_t *)buf;
-      kbd->keydown = 0;
-      kbd->keycode = _KEY_NONE;
+      uint32_t sig = inl(I8042_DATA_PORT);
+      kbd->keydown = (sig & KEYDOWN_MASK) ? 1 : 0;
+      kbd->keycode = (sig & KEYDOWN_MASK) ? (sig | KEYDOWN_MASK) : _KEY_NONE;
       return sizeof(_DEV_INPUT_KBD_t);
     }
   }
