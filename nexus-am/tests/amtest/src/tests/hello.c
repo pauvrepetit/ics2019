cb04540fd970b6e4bfcae05b145b243dcbd00371
tracer-ics2019 <tracer@njuics.org>
2020-10-08 14:47:59 +0800

    >  run U201714761 huao Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  14:47:59 up  3:51,  2 users,  load average: 1.62, 1.06, 0.97 a89606d9b95021376ca88a6477075d341d3c1b7

--- nexus-am/tests/amtest/src/tests/hello.c
+++ nexus-am/tests/amtest/src/tests/hello.c
@@ -7,5 +7,6 @@ static inline void putstr(const char *s) {
 void hello() {
   for (int i = 0; i < 10; i ++) {
     putstr("Hello, AM World @ " __ISA__ "\n");
+    printf("Hello World from printf, %d, %d, %d, %s, %s\n", i, i+1, i+2, "Hu", "Ao");
   }
 }
