b6b5e1aac011a8f492c30877e67235901ce860ec
tracer-ics2019 <tracer@njuics.org>
2020-10-08 15:31:05 +0800

    >  run U201714761 huao Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  15:31:05 up  4:35,  2 users,  load average: 0.72, 0.72, 0.84 84e3d43c18430ef766062ab9cee2ac5a6128ac4

--- nexus-am/am/src/nemu-common/nemu-timer.c
+++ nexus-am/am/src/nemu-common/nemu-timer.c
@@ -2,12 +2,14 @@
 #include <amdev.h>
 #include <nemu.h>
 
+#define RTC_PORT 0x48
+
 size_t __am_timer_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_TIMER_UPTIME: {
       _DEV_TIMER_UPTIME_t *uptime = (_DEV_TIMER_UPTIME_t *)buf;
       uptime->hi = 0;
-      uptime->lo = inl(0x48);
+      uptime->lo = inl(RTC_PORT);
       return sizeof(_DEV_TIMER_UPTIME_t);
     }
     case _DEVREG_TIMER_DATE: {
