0ea2319097b7abd375353560e9edbdb11dfe0cac
tracer-ics2019 <tracer@njuics.org>
2020-10-08 14:49:43 +0800

    >  run U201714761 huao Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  14:49:43 up  3:53,  2 users,  load average: 1.00, 1.09, 0.99 a27b943fba9d2753f85e753c7d9d47aa4fc42db5

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -4,11 +4,11 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  char buf[65536];
-  va_list args;
-  va_start(args, fmt);
-  sprintf(buf, fmt, args);
-  va_end(args);
+  char buf[1024];
+  // va_list args;
+  // va_start(args, fmt);
+  sprintf(buf, fmt);
+  // va_end(args);
   size_t i = 0;
   while (buf[i] != '\0') {
     _putc(buf[i++]);
