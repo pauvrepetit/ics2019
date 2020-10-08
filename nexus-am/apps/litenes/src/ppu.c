b31184c420af2f0e6059b759ce9fe7f9922f2b58
tracer-ics2019 <tracer@njuics.org>
2020-10-08 22:27:22 +0800

    >  run U201714761 huao Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  22:27:22 up  5:54,  2 users,  load average: 1.57, 1.29, 1.16 350cbd7eae6b8d96762cdc3b7b9d5841e3a98e8b

--- nexus-am/apps/litenes/src/ppu.c
+++ nexus-am/apps/litenes/src/ppu.c
@@ -254,8 +254,14 @@ void ppu_cycle() {
   TIME_TYPE t0, t1, t2, t3, t4, t5;
 #endif
 
-  if (!ppu.ready && cpu_clock() > 29658)
+  if (ppu.ready) {
+    printf("ppu.ready\n");
+  } else {
+    printf("!ppu.ready\n");
+  }
+  if (!ppu.ready && cpu_clock() > 29658) {
     ppu.ready = true;
+  }
   
   printf("cpu_clock is %d\n", cpu_clock());
   if (ppu.ready) {
