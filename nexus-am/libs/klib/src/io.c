ef60a3c95403bc98a8b7bc94ac17937dce83536f
tracer-ics2019 <tracer@njuics.org>
2020-10-08 20:57:40 +0800

    >  run U201714761 huao Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  20:57:40 up  4:24,  2 users,  load average: 0.85, 0.82, 0.84 35d2cccaaf109eceb99b142f01179b1d0f260e

--- nexus-am/libs/klib/src/io.c
+++ nexus-am/libs/klib/src/io.c
@@ -32,13 +32,13 @@ void draw_rect(uint32_t *pixels, int x, int y, int w, int h) {
   // memcpy((void *)(uintptr_t)FB_ADDR, (void *)pixels, sizeof(uint32_t) * w * h);
   int width = screen_width();
   int height = screen_height();
-  printf("draw x is %d, y is %d, w is %d, h is %d\n", x, y, w, h);
-  for(int i = 0; i < 0xffffff; i++);
+  // printf("draw x is %d, y is %d, w is %d, h is %d\n", x, y, w, h);
+  // for(int i = 0; i < 0xffffff; i++);
   for (int i = y; (i-y) < h && i < height; i++) {
     memcpy((uint32_t *)(uintptr_t)FB_ADDR + i * width + x, pixels, sizeof(uint32_t) * MIN(w, width-x));
     pixels += w;
   }
-  printf("drawing\n");
+  // printf("drawing\n");
   _io_write(_DEV_VIDEO, _DEVREG_VIDEO_FBCTL, &ctl, sizeof(ctl));
 }
 
