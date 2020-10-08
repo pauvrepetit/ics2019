813c2e903d97bdfd5d51bf8db8435fdae3cdf0e4
tracer-ics2019 <tracer@njuics.org>
2020-10-08 13:39:35 +0800

    >  run U201714761 huao Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  13:39:35 up  2:43,  2 users,  load average: 0.36, 0.64, 0.61 20fd3437f9924abcb50a9ca4a6b99741d31d59

--- nexus-am/tests/cputest/tests/hello-str.c
+++ nexus-am/tests/cputest/tests/hello-str.c
@@ -6,7 +6,7 @@ int main() {
 	sprintf(buf, "%s", "Hello world!\n");
 	nemu_assert(strlen("Hello world!\n") == 13);
 	nemu_assert(strlen(buf) == 13);
-	nemu_assert(buf[10] == 'd');
+	nemu_assert(buf[12] == '\n');
 	// nemu_assert(strcmp(buf, "Hello world!\n") == 0);
 
 	sprintf(buf, "%d + %d = %d\n", 1, 1, 2);
