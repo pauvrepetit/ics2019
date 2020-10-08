42bab1862d64ae2027d5dd2afe319d0197d635cd
tracer-ics2019 <tracer@njuics.org>
2020-10-08 15:38:06 +0800

    >  compile U201714761 huao Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  15:38:06 up  4:42,  2 users,  load average: 1.62, 1.06, 0.92 1b59adb07c0a7881e931d4a16163708e5d4a5f26

--- nexus-am/tests/amtest/src/tests/rtc.c
+++ nexus-am/tests/amtest/src/tests/rtc.c
@@ -6,8 +6,8 @@ void rtc_test() {
   int sec = 1;
   uint32_t uptime_lo;
   while (1) {
-    while((uptime_lo = uptime()) < 1000 * sec)
-      printf("uptime is %08x\n", uptime_lo);
+    while((uptime_lo = uptime()) < 1000 * sec) ;
+      // printf("uptime is %08x\n", uptime_lo);
     get_timeofday(&rtc);
     printf("%d-%d-%d %02d:%02d:%02d GMT (", rtc.year, rtc.month, rtc.day, rtc.hour, rtc.minute, rtc.second);
     if (sec == 1) {
