fe5da44ab8434652499b1f90ea118521baaeaa61
tracer-ics2019 <tracer@njuics.org>
2020-10-09 10:39:31 +0800

    >  compile U201714761 huao Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  10:39:31 up 17 min,  2 users,  load average: 0.80, 0.77, 0.73 4d321f40aea1ee5a850ca304fb8d4ba12a2b2b25

--- nexus-am/tests/amtest/src/tests/hello.c
+++ nexus-am/tests/amtest/src/tests/hello.c
@@ -5,6 +5,10 @@ static inline void putstr(const char *s) {
 }
 
 void hello() {
+  unsigned long long ll = 0x12345;
+  if (ll > 0x1234) {
+    printf("Hello World\n");
+  }
   for (int i = 0; i < 10; i ++) {
     putstr("Hello, AM World @ " __ISA__ "\n");
   }
