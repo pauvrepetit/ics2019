f4fe7299e82a4e93e7a4fc8570691a508e4e55e4
tracer-ics2019 <tracer@njuics.org>
2020-10-09 12:05:15 +0800

    >  run U201714761 huao Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  12:05:15 up  1:43,  2 users,  load average: 0.45, 0.22, 0.27 7897e52855557fa36ef94769c69d33faa013c91

--- nexus-am/tests/amtest/src/tests/hello.c
+++ nexus-am/tests/amtest/src/tests/hello.c
@@ -19,21 +19,6 @@ unsigned long long cpu_clock() {
 }
 
 void hello() {
-  cpu_run(10000);
-  cpu_run(5000);
-  cpu_run(20000);
-  bool ready = false;
-  unsigned int ccl = cpu_clock();
-  if (!ready) {
-    if (ccl > 29658) {
-      printf("???\n");
-      ready = true;
-    }
-  }
-  // if ((!ready) && (cpu_clock() > 29658)) {
-  //   printf("???\n");
-  //   ready = true;
-  // }
   for (int i = 0; i < 10; i ++) {
     putstr("Hello, AM World @ " __ISA__ "\n");
   }
