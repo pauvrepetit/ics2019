98b4030599a6c11d12f84fe20b4094b42fbb908f
tracer-ics2019 <tracer@njuics.org>
2020-10-09 21:06:06 +0800

    >  run U201714761 huao Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  21:06:06 up 10:44,  2 users,  load average: 0.50, 0.51, 0.55 28e1c0c22cb541b799c74aae48c7688fa23f0401

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,7 +2,7 @@
 
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
-    case _EVENT_YIELD: printf("hit event yield, event ID = %d\n", e.event); 
+    case _EVENT_YIELD: printf("hit event yield, event ID = %d\n", e.event); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
