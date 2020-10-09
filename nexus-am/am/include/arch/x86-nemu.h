c8e5b22e44d79ecb505c88de25a9c8205a360fdc
tracer-ics2019 <tracer@njuics.org>
2020-10-09 22:26:45 +0800

    >  run U201714761 huao Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  22:26:45 up 12:05,  2 users,  load average: 0.74, 0.72, 0.77 7dcc378cb2ac57fddd739f5f788112636fe51fc1

--- nexus-am/am/include/arch/x86-nemu.h
+++ nexus-am/am/include/arch/x86-nemu.h
@@ -2,29 +2,16 @@
 #define __ARCH_H__
 
 struct _Context {
-
-  // uintptr_t esp;
   struct _AddressSpace *as;
   uintptr_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
   int irq;
   uintptr_t eip, cs, eflags;
-
-
-  // uintptr_t eflags, cs, eip;
-  // int irq;
-  // uintptr_t eax, ecx, edx, ebx, temp, ebp, esi, edi;
-  // struct _AddressSpace *as;
-  // uintptr_t esp;
-
-  // uintptr_t esi, ebx, eax, eip, edx, eflags, ecx, cs, esp, edi, ebp;
-  // struct _AddressSpace *as;
-  // int irq;
 };
 
 #define GPR1 eax
-#define GPR2 eip
-#define GPR3 eip
-#define GPR4 eip
-#define GPRx eip
+#define GPR2 ebx
+#define GPR3 ecx
+#define GPR4 edx
+#define GPRx eax
 
 #endif
