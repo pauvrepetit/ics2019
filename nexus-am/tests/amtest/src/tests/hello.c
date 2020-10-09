cfaa4436cd46b6735caecb36934e0b37527b47a3
tracer-ics2019 <tracer@njuics.org>
2020-10-09 10:44:18 +0800

    >  run U201714761 huao Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  10:44:18 up 22 min,  2 users,  load average: 0.46, 0.58, 0.66 1a641e7971bf3bb8c339e8d36a56e36d8efe8183

--- nexus-am/tests/amtest/src/tests/hello.c
+++ nexus-am/tests/amtest/src/tests/hello.c
@@ -4,11 +4,27 @@ static inline void putstr(const char *s) {
   for (; *s; s ++) _putc(*s);
 }
 
+typedef int bool;
+#define false 0
+#define true  1
+
+unsigned long long cpu_clock() {
+  return 123456;
+}
+
 void hello() {
-  unsigned long long ll = 0x12345;
-  if (ll > 0x1234) {
-    printf("Hello World\n");
+  
+  bool ready = false;
+  unsigned int ccl = cpu_clock();
+  if (!ready) {
+    if (ccl > 29658) {
+      ready = true;
+    }
   }
+  // if ((!ppu.ready) && (cpu_clock() > 29658)) {
+  //   printf("???\n");
+  //   ppu.ready = true;
+  // }
   for (int i = 0; i < 10; i ++) {
     putstr("Hello, AM World @ " __ISA__ "\n");
   }
