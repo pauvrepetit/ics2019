c21ee1fccf8762c4cfc181fe7e1555d7b53109d8
tracer-ics2019 <tracer@njuics.org>
2020-10-10 10:24:23 +0800

    >  run U201714761 huao Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  10:24:23 up 30 min,  2 users,  load average: 0.36, 0.44, 0.36 932e4dea8d5bca73e089026c2c496aa63a790e40

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -63,7 +63,15 @@ int _write(int fd, void *buf, size_t count) {
 }
 
 void *_sbrk(intptr_t increment) {
-  return (void *)-1;
+  extern intptr_t end;
+  intptr_t old_end = end;
+  int res = _syscall_(SYS_brk, old_end+increment, 0, 0);
+  if (res == 0) {
+    end += increment;
+    return (void *)old_end;
+  } else {
+    return (void *)-1;
+  }
 }
 
 int _read(int fd, void *buf, size_t count) {
