4dab9e4525eb778f41e0b422e1345b74d35aa683
tracer-ics2019 <tracer@njuics.org>
2020-10-10 18:15:04 +0800

    >  run U201714761 huao Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  18:15:04 up  8:20,  2 users,  load average: 0.88, 0.96, 1.00 3fbda5e3a1f91fae27aa3e7b69241e9753596ab8

--- navy-apps/libs/libndl/src/ndl.c
+++ navy-apps/libs/libndl/src/ndl.c
@@ -129,11 +129,13 @@ int NDL_WaitEvent(NDL_Event *event) {
 }
 
 static void get_display_info() {
+  printf("getdisplayinfo\n");
   FILE *dispinfo = fopen("/proc/dispinfo", "r");
   assert(dispinfo);
   screen_w = screen_h = 0;
   char buf[128], key[128], value[128], *delim;
   while (fgets(buf, 128, dispinfo)) {
+    printf("buf is '%s'\n", buf);
     *(delim = strchr(buf, ':')) = '\0';
     sscanf(buf, "%s", key);
     sscanf(delim + 1, "%s", value);
