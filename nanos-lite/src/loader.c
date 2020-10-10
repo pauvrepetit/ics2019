b38ee75cc5ede56439e2db333ccec2179664277b
tracer-ics2019 <tracer@njuics.org>
2020-10-10 14:35:39 +0800

    >  compile U201714761 huao Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  14:35:39 up  4:41,  2 users,  load average: 1.04, 0.95, 0.79 4246ed5eb37af51c66550666eae95a384229228

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -44,7 +44,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 
 void naive_uload(PCB *pcb, const char *filename) {
   uintptr_t entry = loader(pcb, filename);
-  Log("Jump to entry = %d", entry);
+  printf("load finish %d\n", entry);
+  // Log("Jump to entry = %d", entry);
   ((void(*)())entry) ();
 }
 
