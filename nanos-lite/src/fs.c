288a9ee23027b02cb6c43920923b7495182b5d4f
tracer-ics2019 <tracer@njuics.org>
2020-10-10 15:44:31 +0800

    >  run U201714761 huao Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  15:44:31 up  5:50,  3 users,  load average: 1.40, 0.86, 0.77 cfc839464c089c8f1b8f5ee75836a452c1c514f

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -40,7 +40,9 @@ size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 
 int fs_open(const char *pathname, int flags, int mode) {
+  printf("open file %s\n", pathname);
   for (int i = 0; i < NR_FILES; i++) {
+    printf("compare to %s\n", file_table[i].name);
     if (strcmp(pathname, file_table[i].name) == 0) {
       file_table[i].open_offset = 0;
       return i;
