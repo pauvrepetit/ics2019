ed09a99f0d6d04b1818d983ef0d18d426377a224
tracer-ics2019 <tracer@njuics.org>
2020-10-10 18:25:53 +0800

    >  run U201714761 huao Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  18:25:53 up  8:31,  2 users,  load average: 0.54, 0.69, 0.84 977bc4c7acb73f2d1150c9c2af3c4078511c059

--- navy-apps/libs/libndl/src/ndl.c
+++ navy-apps/libs/libndl/src/ndl.c
@@ -140,6 +140,8 @@ static void get_display_info() {
     printf("buf is '%s'\n", buf);
     sscanf(buf, "%s", key);
     sscanf(delim + 1, "%s", value);
+    printf("key is '%s'\n", key);
+    printf("value is '%s'\n", value);
     if (strcmp(key, "WIDTH") == 0) sscanf(value, "%d", &screen_w);
     if (strcmp(key, "HEIGHT") == 0) sscanf(value, "%d", &screen_h);
   }
