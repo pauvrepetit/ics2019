5eb37ac5ef38c10664607239f29239434b767ec1
tracer-ics2019 <tracer@njuics.org>
2020-10-10 15:08:26 +0800

    >  run U201714761 huao Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  15:08:26 up  5:14,  3 users,  load average: 0.92, 0.84, 0.75 e2e7ca84b1986d6b00d7c6bbbcc0a8ac2ed0b2

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -59,18 +59,20 @@ size_t fs_read(int fd, void *buf, size_t len) {
 }
 
 size_t fs_write(int fd, const void *buf, size_t len) {
+  if (fd == FD_STDIN) {
+    return 0;
+  } else if (fd == FD_STDOUT || fd == FD_STDERR) {
+    int count = 0;
+    for (count = 0; count < len; count++) {
+      _putc(((const char *)buf)[count]);
+    }
+    return count;
+  }
   Finfo *f = file_table + fd;
   if (f->open_offset + len >= f->size) {
     len = f->size - f->open_offset;
   }
-  // _putc('?');
-  // _putc('?');
-  if (fd == FD_STDERR) {
-    return -1;
-  }
-  printf("??? %d\n", fd);
   len = ramdisk_write(buf, f->disk_offset + f->open_offset, len);
-  _putc('!');
   f->open_offset += len;
   return len;
 }
