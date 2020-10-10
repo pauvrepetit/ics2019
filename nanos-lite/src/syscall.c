13d8747c890d82eee7259808ac8e52c35a368141
tracer-ics2019 <tracer@njuics.org>
2020-10-10 14:59:15 +0800

    >  run U201714761 huao Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  14:59:15 up  5:05,  3 users,  load average: 0.63, 0.71, 0.67 1ff372313571bf44f0aef2e92452261bb90649f8

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -24,9 +24,9 @@ _Context* do_syscall(_Context *c) {
     // case SYS_close:
     //   c->GPRx = fs_close(a[1]);
     //   break;
-    // case SYS_lseek:
-    //   c->GPRx = fs_lseek(a[1], a[2], a[3]);
-    //   break;
+    case SYS_lseek:
+      c->GPRx = fs_lseek(a[1], a[2], a[3]);
+      break;
     // case SYS_write: {
     //   if (a[1] != 1 && a[1] != 2) {
     //     c->GPRx = -1;
