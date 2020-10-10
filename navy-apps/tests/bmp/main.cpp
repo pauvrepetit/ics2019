316655de57bd4129bf731412abb1898a4c766c72
tracer-ics2019 <tracer@njuics.org>
2020-10-10 18:59:40 +0800

    >  run U201714761 huao Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  18:59:40 up  9:05,  2 users,  load average: 0.48, 0.56, 0.69 f4fbf96e27ea2eacf534eed5dfb7f45272f06cc3

--- navy-apps/tests/bmp/main.cpp
+++ navy-apps/tests/bmp/main.cpp
@@ -6,19 +6,14 @@
 
 int main() {
   NDL_Bitmap *bmp = (NDL_Bitmap*)malloc(sizeof(NDL_Bitmap));
-  printf("111\n");
   NDL_LoadBitmap(bmp, "/share/pictures/projectn.bmp");
-  printf("222\n");
   assert(bmp->pixels);
-  printf("333\n");
   NDL_OpenDisplay(bmp->w, bmp->h);
-  printf("444\n");
+  printf("w is %d, h is %d\n", bmp->w, bmp->h);
   NDL_DrawRect(bmp->pixels, 0, 0, bmp->w, bmp->h);
   printf("555\n");
   NDL_Render();
-  printf("666\n");
   NDL_CloseDisplay();
-  printf("777\n");
   while (1);
   return 0;
 }
