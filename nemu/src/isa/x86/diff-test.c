d221b897043001c26e7319af51cd9b1c5e89a833
tracer-ics2019 <tracer@njuics.org>
2020-10-14 11:15:18 +0800

    >  compile U201714761 huao Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  11:15:18 up  1:30,  3 users,  load average: 0.84, 0.90, 0.81 5442989952fd1f3f3a5d422a9d93f59eaabcc0be

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -16,6 +16,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
 }
 
 void isa_difftest_attach(void) {
+  printf("here1\n");
   ref_difftest_memcpy_from_dut(0, 0, 0x7c00);
   printf("here2\n");
 
