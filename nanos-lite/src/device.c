daa9758f23e8a998934d146154d14792e9c413f6
tracer-ics2019 <tracer@njuics.org>
2020-10-14 11:49:02 +0800

    >  run U201714761 huao Linux archlinux 5.8.13-arch1-1 #1 SMP PREEMPT Thu, 01 Oct 2020 20:40:35 +0000 x86_64 GNU/Linux  11:49:02 up  2:04,  2 users,  load average: 1.05, 1.00, 1.09 59d5db94f5d8bbc9913ca6f8904f95986649e715

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -25,7 +25,6 @@ size_t events_read(void *buf, size_t offset, size_t len) {
     _DEV_TIMER_UPTIME_t time_event;
     _io_read(_DEV_TIMER, _DEVREG_TIMER_UPTIME, &time_event, sizeof(_DEV_TIMER_DATE_t));
     sprintf((char *)buf, "t %d\n", time_event.lo);
-    printf("hit kbd\n");
   } else {
     // 有键盘事件，我们输出键盘事件
     if (kbd_event.keydown == 1) {
@@ -34,6 +33,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
     } else {
       sprintf((char *)buf, "ku %s\n", keyname[kbd_event.keycode & 0x7fff]);
     }
+    printf("hit kbd %s\n", keyname[kbd_event.keycode & 0x7fff]);
   }
   return strlen((char *)buf);
 }
