5c454aff52e91170eced7c63964369d79fb5e173
tracer-ics2019 <tracer@njuics.org>
2020-11-02 15:26:00 +0800

    >  compile U201714761 huao Linux archlinux 5.9.2-arch1-1 #1 SMP PREEMPT Thu, 29 Oct 2020 17:01:28 +0000 x86_64 GNU/Linux  15:26:00 up  1:54,  2 users,  load average: 0.57, 0.57, 0.56 370564fa7ba77c5c3c73f502cad8743df3c6fee0

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -1,11 +1,48 @@
 #include "nemu.h"
 
+// Page directory and page table constants
+#define PGSIZE         4096    // Bytes mapped by a page
+#define PGMASK         4095    // Mask for bit ops
+#define NR_PDE         1024    // # directory entries per page directory
+#define NR_PTE         1024    // # PTEs per page table
+#define PGSHFT         12      // log2(PGSIZE)
+#define PTXSHFT        12      // Offset of PTX in a linear address
+#define PDXSHFT        22      // Offset of PDX in a linear address
+
+// Page table/directory entry flags
+#define PTE_P          0x001   // Present
+#define PTE_W          0x002   // Writeable
+#define PTE_U          0x004   // User
+#define PTE_PWT        0x008   // Write-Through
+#define PTE_PCD        0x010   // Cache-Disable
+#define PTE_A          0x020   // Accessed
+#define PTE_D          0x040   // Dirty
+
+typedef uint32_t PTE;
+typedef uint32_t PDE;
+#define PDX(va)          (((uint32_t)(va) >> PDXSHFT) & 0x3ff)
+#define PTX(va)          (((uint32_t)(va) >> PTXSHFT) & 0x3ff)
+#define OFF(va)          ((uint32_t)(va) & 0xfff)
+#define ROUNDUP(a, sz)   ((((uintptr_t)a)+(sz)-1) & ~((sz)-1))
+#define ROUNDDOWN(a, sz) ((((uintptr_t)a)) & ~((sz)-1))
+#define PTE_ADDR(pte)    ((uint32_t)(pte) & ~0xfff)
+#define PGADDR(d, t, o)  ((uint32_t)((d) << PDXSHFT | (t) << PTXSHFT | (o)))
+
+
 paddr_t page_translate(vaddr_t vaddr) {
   // 将虚地址转换为实地址
-  uint32_t* pgdir = cpu.cr3;
-  uint32_t* pgtable = pgdir[PDX(vaddr)];
-  // if (pgtable & )
-  uint32_t pgentry = pgtable[PTX(vaddr)];
+  PTE pgdir = cpu.cr3;
+  PDE pgtable = ((uint32_t*)pgdir)[PDX(vaddr)];
+  if (!(pgtable & PTE_P)) {
+    // 页不存在
+    assert(0);
+  }
+  pgtable = PTE_ADDR(pgtable);
+  uint32_t pgentry = ((uint32_t*)pgtable)[PTX(vaddr)];
+  if (!(pgentry & PTE_P)) {
+    assert(0);
+  }
+  pgentry = PTE_ADDR(pgentry);
   return pgentry + OFF(vaddr);
 }
 
