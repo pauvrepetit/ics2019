838a051b521a726b31b0b4b6f67e67dde7409515
tracer-ics2019 <tracer@njuics.org>
2020-11-02 15:19:54 +0800

    >  compile U201714761 huao Linux archlinux 5.9.2-arch1-1 #1 SMP PREEMPT Thu, 29 Oct 2020 17:01:28 +0000 x86_64 GNU/Linux  15:19:54 up  1:48,  2 users,  load average: 0.27, 0.39, 0.52 81a9749069b71b2bfc9cb01b1dc3538491f8d818

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -1,9 +1,32 @@
 #include "nemu.h"
 
+paddr_t page_translate(vaddr_t vaddr) {
+  // 将虚地址转换为实地址
+  uint32_t* pgdir = cpu.cr3;
+  uint32_t* pgtable = pgdir[PDX(vaddr)];
+  // if (pgtable & )
+  uint32_t pgentry = pgtable[PTX(vaddr)];
+  return pgentry + OFF(vaddr);
+}
+
 uint32_t isa_vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  // return paddr_read(addr, len);
+  if ((addr & (~PAGE_MASK)) != ((addr + len) & (~PAGE_MASK))) {
+    // 访问的数据段跨越了页
+    assert(0);
+  } else {
+    paddr_t paddr = page_translate(addr);
+    return paddr_read(paddr, len);
+  }
 }
 
 void isa_vaddr_write(vaddr_t addr, uint32_t data, int len) {
-  paddr_write(addr, data, len);
+  // paddr_write(addr, data, len);
+  if ((addr & (~PAGE_MASK)) != ((addr + len) & (~PAGE_MASK))) {
+    // 访问的数据段跨越了页
+    assert(0);
+  } else {
+    paddr_t paddr = page_translate(addr);
+    return paddr_write(paddr, data, len);
+  }
 }
