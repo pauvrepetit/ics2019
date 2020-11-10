6e1c5edbd62821cea4c8c299b04cf83bdf09752e
tracer-ics2019 <tracer@njuics.org>
2020-11-10 16:07:04 +0800

    >  run U201714761 huao Linux archlinux 5.9.2-arch1-1 #1 SMP PREEMPT Thu, 29 Oct 2020 17:01:28 +0000 x86_64 GNU/Linux  16:07:04 up  3:39,  2 users,  load average: 0.55, 0.96, 0.90 99872411c27c42dd8540956f904362b3b9075d16

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -6,9 +6,6 @@ static void *pf = NULL;
 void* new_page(size_t nr_page) {
   void *p = pf;
   pf += PGSIZE * nr_page;
-  if (pf >= (void *)_heap.end) {
-    printf("pf is %d\n", pf);
-  }
   assert(pf < (void *)_heap.end);
   return p;
 }
@@ -26,7 +23,6 @@ int mm_brk(uintptr_t brk, intptr_t increment) {
   if (current->max_brk < brk + increment) {
     int block_count = (brk + increment - current->max_brk) / PGSIZE;
     if ((brk + increment - current->max_brk) % PGSIZE) block_count++;
-    printf("brk is %d, increment is %d, block_count is %d\n",brk, increment, block_count);
     for (int i = 0; i < block_count; i++) {
       _map(&current->as, current->max_brk, 0, 0);
       current->max_brk += PGSIZE;
