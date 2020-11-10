e1a8949260ef04585e751d74530dd7bbe6fdb332
tracer-ics2019 <tracer@njuics.org>
2020-11-10 14:12:01 +0800

    >  run U201714761 huao Linux archlinux 5.9.2-arch1-1 #1 SMP PREEMPT Thu, 29 Oct 2020 17:01:28 +0000 x86_64 GNU/Linux  14:12:01 up  1:44,  3 users,  load average: 1.19, 0.82, 0.75 43bc1d2b30f4107ffe0d8edc56bc91d2f3dd27c5

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -33,6 +33,7 @@ int main() {
   Log("Finish initialization");
 
 #ifdef HAS_CTE
+  printf("prepare to _yield\n");
   _yield();
 #endif
 
--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,7 +26,6 @@ void init_proc() {
   // context_kload(&pcb[0], (void *)hello_fun);
   context_uload(&pcb[0], "/bin/dummy");
   switch_boot_pcb();
-  printf("here\n");
 
   // _yield();
 
