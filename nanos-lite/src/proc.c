e49c169baf4483ffcda299a7b1ddeecdc13b24db
tracer-ics2019 <tracer@njuics.org>
2020-11-14 19:18:17 +0800

    >  run U201714761 huao Linux archlinux 5.9.2-arch1-1 #1 SMP PREEMPT Thu, 29 Oct 2020 17:01:28 +0000 x86_64 GNU/Linux  19:18:17 up  1:07,  2 users,  load average: 0.61, 0.65, 0.73 2726f5d026e146b1aa827bd9b986f233640c299a

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -23,17 +23,14 @@ void context_kload(PCB *pcb, void *entry);
 void context_uload(PCB *pcb, const char *filename);
 
 void init_proc() {
+  Log("Initializing processes...");
   // context_kload(&pcb[0], (void *)hello_fun);
   // context_uload(&pcb[0], "/bin/hello");
   context_uload(&pcb[0], "/bin/init");
   switch_boot_pcb();
 
   // _yield();
-
-  // switch_boot_pcb();
-
-  // Log("Initializing processes...");
-
+  
   // load program here
   // naive_uload(NULL, "/bin/init");
 }
@@ -46,15 +43,14 @@ _Context* schedule(_Context *prev) {
   current->cp = prev; // 这个操作很重要，prev是当前进程的上下文，他在栈里面一个不确定的位置，因此在进行上下文切换的时候我们需要保存当前进程上下文的位置，保存到该进程的PCB中，也就是current中，current只是一个指针，下面把这个指针指向另一个PCB就可以执行另一个进程了
   // 而下次进程调度如果调度到当前进程的话，我们就可以在栈中找到其被中断时的上下文
   // current = pcb;  // current指向第0个pcb进程
-  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
-
-  // int current_id = current - pcb;
-  // for (int i = 1; i <= MAX_NR_PROC; i++) {
-  //   int next_id = (i + current_id) % MAX_NR_PROC;
-  //   if (pcb[next_id].exist == 1) {
-  //     current = &pcb[next_id];
-  //     break;
-  //   }
-  // }
+  // current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+  int current_id = current - pcb;
+  for (int i = 1; i <= MAX_NR_PROC; i++) {
+    int next_id = (i + current_id) % MAX_NR_PROC;
+    if (pcb[next_id].exist == 1) {
+      current = &pcb[next_id];
+      break;
+    }
+  }
   return current->cp;
 }
