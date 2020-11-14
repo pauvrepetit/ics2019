aae604b8b92012bff18af99cb38728e544ea8035
tracer-ics2019 <tracer@njuics.org>
2020-11-14 20:55:23 +0800

    >  run U201714761 huao Linux archlinux 5.9.2-arch1-1 #1 SMP PREEMPT Thu, 29 Oct 2020 17:01:28 +0000 x86_64 GNU/Linux  20:55:23 up  2:44,  2 users,  load average: 0.69, 1.02, 1.00 186c5cb015cd9bef3cae4c2ae09f8b005dfa6753

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -25,12 +25,10 @@ void context_uload(PCB *pcb, const char *filename);
 void init_proc() {
   Log("Initializing processes...");
   // context_kload(&pcb[0], (void *)hello_fun);
-  // context_uload(&pcb[0], "/bin/hello");
-  // context_uload(&pcb[2], "/bin/init");
-  // context_uload(&pcb[4], "/bin/init");
-  // context_uload(&pcb[6], "/bin/init");
-
-  context_uload(&pcb[0], "/bin/init");
+  context_uload(&pcb[0], "/bin/hello");
+  context_uload(&pcb[2], "/bin/init");
+  context_uload(&pcb[4], "/bin/init");
+  context_uload(&pcb[6], "/bin/init");
   switch_boot_pcb();
 
   // _yield();
@@ -44,17 +42,31 @@ void context_uload_sys(const char *filename) {
   context_uload(current+1, filename);
 }
 
+extern int fg_pcb;
 _Context* schedule(_Context *prev) {
   current->cp = prev; // 这个操作很重要，prev是当前进程的上下文，他在栈里面一个不确定的位置，因此在进行上下文切换的时候我们需要保存当前进程上下文的位置，保存到该进程的PCB中，也就是current中，current只是一个指针，下面把这个指针指向另一个PCB就可以执行另一个进程了
   // 而下次进程调度如果调度到当前进程的话，我们就可以在栈中找到其被中断时的上下文
   // current = pcb;  // current指向第0个pcb进程
   // current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
 
-  if (pcb[1].exist) {
-    current = &pcb[1];
-  } else {
-    current = pcb;
+  switch (fg_pcb) {
+    case 0:
+      current = pcb;
+      break;
+    default:
+      if (pcb[fg_pcb+1].exist) {
+        current = &pcb[fg_pcb+1];
+      } else {
+        current = &pcb[fg_pcb];
+      }
+      break;
   }
+
+  // if (pcb[1].exist) {
+  //   current = &pcb[1];
+  // } else {
+  //   current = pcb;
+  // }
 
   // int current_id = current - pcb;
   // for (int i = 1; i <= MAX_NR_PROC; i++) {
